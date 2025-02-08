04c084a0d47d68341ed00f6cd42b30e47e69bef0
satyammthakur <satyammthakur@gmail.com>
2025-02-08 12:37:42 +0530

    started with my views folder

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 import express from "express";
 // import urlRoute from "./routes/url.route.js";
 import dotenv from "dotenv";
+import path from "path";
 import { connectToMongoDB } from "./connect.js";
 // import URL from "../models/url.model.js";
 import URL from "./models/url.model.js";
+import urlRoute from "./routes/url.route.js";
+import staticRouter from "./routes/static.route.js";
 dotenv.config();
 const app = express();
 app.use(express.json()); 
-import urlRoute from "./routes/url.route.js";
+
 app.use("/url", urlRoute);
+app.use("/" , staticRouter);
 
 const PORT = 8001;
 // app.use("/url" , urlRoute);
 // app.use(express.json());
+
+app.set("view engine" , "ejs" );
+app.set('views' , path.resolve("./views"));
+
+
+app.get("/test" , async (req,res)=>{    
+    const allURLs = await URL.find({});
+    return res.render("home" , {
+        urls: allURLs,
+    });
+})
 app.get('/:shortId' , async (req,res)=>{
     const shortId = req.params.shortId;
     const entry = await URL.findOneAndUpdate({
@@ -24,6 +39,9 @@ app.get('/:shortId' , async (req,res)=>{
             },
         },
     })
+    if (!entry) {
+        return res.status(404).json({ error: 'Short URL not found' });
+    }
     res.redirect(entry.redirectURL);
 })
 
